6f29b4859dac1c97b87249c7d8f42b52bdb4cc54
huanghao-code <huanghao_bupt@sina.com>
2017-08-22 21:46:28 -0400

    Create a217.h

---
+++ a217.h
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        	sort(nums.begin(), nums.end());
+          vector<int>::iterator rep = unique(nums.begin(), nums.end());
+          if(rep - nums.begin() == nums.end() - nums.begin()){
+            return false;
+          }else{
+            return true;
+          }
+    }
+};
