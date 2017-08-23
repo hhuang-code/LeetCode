db7f6ec6e86b0ebcd5cfc225925d7fd6545bdbce
huanghao-code <huanghao_bupt@sina.com>
2017-08-22 21:46:52 -0400

    Update a217.h

--- a217.h
+++ a217.h
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        	sort(nums.begin(), nums.end());
+          sort(nums.begin(), nums.end());
           vector<int>::iterator rep = unique(nums.begin(), nums.end());
           if(rep - nums.begin() == nums.end() - nums.begin()){
             return false;
