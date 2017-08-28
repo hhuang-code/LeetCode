fbbfac91222daba147760d93792a3c635ef96178
huanghao-code <huanghao_bupt@sina.com>
2017-08-28 13:40:39 -0400

    Create a507.h

---
+++ a507.h
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num <= 0){
+            return false;
+        }
+        
+        int sum = 0;
+        for(int i = 1; i * i <= num; i++){
+            if(!(num % i)){
+                sum += i;
+                sum += num / i;
+                if(i * i == num){
+                    sum -= i;
+                }
+            }
+        }
+        
+        return sum - num == num;
+    }
+};
